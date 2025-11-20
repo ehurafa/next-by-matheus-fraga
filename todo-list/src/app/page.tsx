912082f296bfbb618b06c3aa1b340adb9c60a1cb
Rafael Gomes <rafarfl2010@gmail.com>
2025-11-20 20:42:48 -0300

    feat(styles): applying Home styles

--- todo-list/src/app/page.tsx
+++ todo-list/src/app/page.tsx
@@ -1,7 +1,27 @@
+import Head from 'next/head'
+import styles from "../styles/home.module.css"
+import Image from 'next/image'
+
+import heroImage from '../../public/assets/hero.png' 
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>TODO List</h1>
+    <div className={styles.container}>
+      <Head>
+        <title>Tarefas+ | Organize sua tarefas de forma fácil</title>
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo Tarefas+"
+            src={heroImage}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>Sistema feito para você organizar <br/>seus estudos e tarefas</h1>
+      </main>
     </div>
   );
 }
